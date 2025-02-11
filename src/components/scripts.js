4069badd6acf700ac03b981d7d0ff895b46755ac
Zachary-Sheen <zac.t.sheen@gmail.com>
2025-02-11 08:59:04 -0700

    Minor changes for better functionality.

--- src/components/scripts.js
+++ src/components/scripts.js
@@ -5,3 +5,28 @@
 // function toggleMenu() { #FIX BUTTON GIVES ERROR RIGHT NOW
 //     document.querySelector('#sidebar').classList.toggle('collapsed');
 // }
+// function App() {
+    // const [count,setCount] = React.useState(0);
+
+    // function Increment() {
+    //     setCount(count + 1);}
+    // function Decrement() {
+    //     setCount(count - 1);}
+    // return (
+    //     <div>
+    //         <h1>{count}</h1>
+    //         <button onClick={Increment}>Increment</button>
+    //         <button onClick={Decrement}>Decrement</button>
+    //     </div>
+//}
+// dict example
+// const dict = {
+//     first: 'first',
+//     second: 'second',
+//     third: 'third',
+//     f: function() {
+//         console.log('f');
+//     }
+// };
+// Adding a new key-value pair
+// dict.fourth = 'fourth';
